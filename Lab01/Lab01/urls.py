e6a96fcb8e958047250c1b7f47886448fe50a09c
OstrowskaBeata <100704152+OstrowskaBeata@users.noreply.github.com>
2022-11-10 15:16:36 +0100

    lab5

--- Lab01/Lab01/urls.py
+++ Lab01/Lab01/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('testowaAnkieta/osoby', include('rest_framework.urls')),
+    path('testowaAnkieta/druzyny', include('rest_framework.urls')),
 ]
